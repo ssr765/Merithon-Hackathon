0e82d08a3c23c7397ea57c287d547db048ce70cd
ssr765 <sergi.serra.r@gmail.com>
2024-04-27 10:12:03 +0200

    [INIT] Frontend

---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+</script>
+
+<template>
+  <main>
+    <div>
+      <Button>works</Button>
+    </div>
+  </main>
+</template>
