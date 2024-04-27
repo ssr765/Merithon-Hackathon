27f95a91199b0a6695249a84d0e8b49b5486dd44
ssr765 <sergi.serra.r@gmail.com>
2024-04-27 14:45:32 +0200

    CORS solved

--- backend/API.js
+++ backend/API.js
@@ -1,4 +1,3 @@
-
 const express = require("express");
 const cors = require("cors");
 const ProcesarTexto = require("./routes/ProcesarTexto.routes");
@@ -7,14 +6,13 @@ const mostrarRutas = require("./utils/index");
 const app = express();
 const port = 3000;
 
+app.use(cors());
 app.use("/api", ProcesarTexto);
-app.use(cors({ origin: "http://localhost:5173" }));
-
 
 console.log("##### RUTAS #####");
 mostrarRutas(app);
 console.log("### FIN RUTAS ###");
 
 app.listen(port, () => {
-    console.log(`Running on port ${port}`);
+  console.log(`Running on port ${port}`);
 });
