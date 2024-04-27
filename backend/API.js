8aeb2c543dbe2e21649ba885dda5991f81a9bb69
Salah Eddin <salah.extras@gmail.com>
2024-04-27 15:30:03 +0200

    pushh

--- backend/API.js
+++ backend/API.js
@@ -3,13 +3,19 @@ const express = require("express");
 const cors = require("cors");
 const ProcesarTexto = require("./routes/ProcesarTexto.routes");
 const mostrarRutas = require("./utils/index");
+const bodyParser = require('body-parser')
 
 const app = express();
 const port = 3000;
 
-app.use("/api", ProcesarTexto);
-app.use(cors({ origin: "http://localhost:5173" }));
 
+app.use(express.json());
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+
+// routes 
+app.use("/api", ProcesarTexto);
 
 console.log("##### RUTAS #####");
 mostrarRutas(app);
--- backend/routes/ProcesarTexto.routes.js
+++ backend/routes/ProcesarTexto.routes.js
@@ -1,10 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const fs = require('fs')
+const multer = require('multer');
+const upload = multer();
+
+// const OpenAI = require("openai");
+// const openai = new OpenAI();
 
 module.exports = router
 
     .post('/procesar', (req, res) => {
+        const audioFile = req.body
+        console.log(audioFile)
+        // async function transcribir() {
+        //     const transcription = await openai.audio.transcriptions.create({
+        // file: fs.createReadStream("/path/to/file/speech.mp3"),
+        // model: "whisper-1",
+        // response_format: "text",
+        // });
+        // console.log(transcription.text);
+        // }
 
-        console.log(req.body)
-        res.send('Procesar Texto')
+        // transcribir();
+        res.json({ message: 'Procesar Texto' })
     })
+
+
+
+
+
