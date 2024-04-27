ecfe147a9331b91edb70d2d733918aafa45949a5
Salah Eddin <salah.extras@gmail.com>
2024-04-27 12:33:33 +0200

    cors intalado

--- backend/API.js
+++ backend/API.js
@@ -1,11 +1,20 @@
 const express = require('express')
-
+const cors = require('cors')
 const ProcesarTexto = require('./routes/ProcesarTexto.routes')
+
+
+
+
+
 
 const app = express()
 const port = 3000
 
 app.use('/api', ProcesarTexto)
+app.use(cors({origin: 'http://localhost:5173'}))
+
+
+
 
 app.listen(port, () => {
     console.log(`Running on port ${port}`)
