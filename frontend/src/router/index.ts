3b13ce7118288a146f73c8aca4dc59a1bab45f9c
ssr765 <sergi.serra.r@gmail.com>
2024-04-27 18:42:12 +0200

    Redirect en la raíz

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -11,7 +11,7 @@ const router = createRouter({
         {
           path: '',
           name: 'home',
-          component: () => import('@/views/HomeView.vue')
+          redirect: { name: 'records-create' }
         },
         {
           path: 'records',
