5be47e071acdf091fc33e39dd8d52cf657e4c333
ssr765 <sergi.serra.r@gmail.com>
2024-04-27 12:50:39 +0200

    Mostrar las rutas del backend

--- backend/API.js
+++ backend/API.js
@@ -1,21 +1,18 @@
-const express = require('express')
-const cors = require('cors')
-const ProcesarTexto = require('./routes/ProcesarTexto.routes')
+const express = require("express");
+const cors = require("cors");
+const ProcesarTexto = require("./routes/ProcesarTexto.routes");
+const mostrarRutas = require("./utils/index");
 
+const app = express();
+const port = 3000;
 
+app.use("/api", ProcesarTexto);
+app.use(cors({ origin: "http://localhost:5173" }));
 
-
-
-
-const app = express()
-const port = 3000
-
-app.use('/api', ProcesarTexto)
-app.use(cors({origin: 'http://localhost:5173'}))
-
-
-
+console.log("##### RUTAS #####");
+mostrarRutas(app);
+console.log("### FIN RUTAS ###");
 
 app.listen(port, () => {
-    console.log(`Running on port ${port}`)
-})
+  console.log(`Running on port ${port}`);
+});
